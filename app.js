02d47b3398fd8d44f5522e2608aa125767ea687f
VladVorniceanu <vorniceanu.v@gmail.com>
2023-12-20 19:17:00 +0200

    DONE for today 20.12.2023

--- app.js
+++ app.js
@@ -3,23 +3,17 @@ document.addEventListener('DOMContentLoaded', function() {
     fetchAndFormatData();
 });
 
+
 async function fetchAndFormatData() {
     try {
-
         const dateSperantaViata = await fetchDataEurostat('demo_mlexpec?sex=T&age=Y1');
-
         const datePopulatie = await fetchDataEurostat('demo_pjan?sex=T&age=TOTAL');
-
         const datePIB = await fetchDataEurostat('sdg_08_10?na_item=B1GQ&unit=CLV10_EUR_HAB');
-
-        
-
-        console.log('Datele au fost formatate și salvate cu succes în media/dateEuroStat.json');
+    
     } catch (error) {
         console.error('Eroare la preluarea sau formatarea datelor:', error);
     }
 }
-
 async function fetchDataEurostat(setDate) { // functie in care imi construiesc apelul API si lansez cererea, apoi returnez datele
     const urlBaza = 'https://ec.europa.eu/eurostat/api/dissemination/statistics/1.0/data/';
     const tari = ['AT', 'BE', 'BG', 'CZ', 'CY', 'DE', 'DK', 'EE', 'EL', 'ES', 'FI', 'FR', 'HR', 'HU', 'IE', 'IT', 'LT', 'LU', 'LV', 'MT', 'NL', 'PL', 'PT', 'RO', 'SE', 'SI', 'SK'];
